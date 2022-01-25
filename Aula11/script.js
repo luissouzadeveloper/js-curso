806a23fef71f7d77150b458e23fd5728715214c1
Luis Felipe <luisfelipeosouzati@gmail.com>
2022-01-25 20:25:56 -0300

    Bloco de comandos

---
+++ Aula11/script.js
@@ -0,0 +1,7 @@
+var num = 20;
+
+if (num > 10) {
+    document.write(`Maior que 10`); // <- Isso é um bloco (entre chaves!)
+} else {
+    document.write(`Menor que 10`); // <- Esse já é outro bloco!
+}
