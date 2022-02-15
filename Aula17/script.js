cfd24bbbe10b7da8a9a5a4e55cc396987a30a8a1
Luis Felipe <luisfelipeosouzati@gmail.com>
2022-02-15 09:39:38 -0300

    Incremento e decremento

---
+++ Aula17/script.js
@@ -0,0 +1,16 @@
+var num = 10;
+/*num = num * 2;*/
+//++num; //<- Forma contraida
+
+//num+=10
+
+document.write(++num);
+document.write('<br>');
+document.write(num + '<br>');
+
+var num2 = 10;
+
+//num2*=-1;
+
+document.write(-num2 + '<br>');
+document.write(num2)
