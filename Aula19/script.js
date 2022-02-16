efac749e58206baf3b3d1b5c3dafa074cb9fc715
Luis Felipe <luisfelipeosouzati@gmail.com>
2022-02-16 10:44:32 -0300

    Laço while

---
+++ Aula19/script.js
@@ -0,0 +1,28 @@
+var contador;
+
+contador = 0;
+
+while(contador < 10){
+    document.write(contador + '<br>');
+    contador++
+}
+
+document.write('<hr>');
+
+var ate;
+
+contador = 10;
+
+/*ate = Number(prompt('Até onde você deseja contar? '));
+
+while (contador < ate) {
+    document.write(contador + '<br>');
+    contador++;
+}*/
+
+document.write('<hr>');
+
+while(contador > 0){
+    document.write(contador + '<br>');
+    contador--;
+}
