24d22e6db02cb5a57a7cc7bcb30bbc4eec211cbb
Luis Felipe <luisfelipeosouzati@gmail.com>
2022-02-17 20:53:24 -0300

    Laço do while

---
+++ Aula20/script.js
@@ -0,0 +1,14 @@
+var num = 0;
+
+/*while (num < 20) {
+
+    O while testa antes e executa depois!
+
+    document.write(num + '<br>');
+    num++;
+}*/
+
+do{
+    document.write(num + '<br>');
+    num++;                          // Do while executa primeiro e depois testa!
+}while(num < 10);
