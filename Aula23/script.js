d2f1d24817faa9e80ae7eb81c1d571c5758b0084
Luis Felipe <luisfelipeosouzati@gmail.com>
2022-02-22 21:51:05 -0300

    Manipulando elementos html e css pelo js

---
+++ Aula23/script.js
@@ -0,0 +1,10 @@
+var txt = document.getElementById('texto');
+txt.style.color = 'red';
+txt.style.fontSize = '30px';
+
+var cx = document.getElementById('caixa');
+cx.style.border = '1px solid black';
+cx.style.width = '200px';
+cx.style.height = '200px';
+cx.style.backgroundColor = 'gray';
+cx.style.margin = 'auto';
