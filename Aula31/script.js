3e36cb01cfd29dbe72aae2b611b9d1a2a1f10326
Luis Felipe <luisfelipeosouzati@gmail.com>
2022-03-15 09:06:11 -0300

    Escopo de variáveis

---
+++ Aula31/script.js
@@ -0,0 +1,10 @@
+var nomeCor = 'Vermelho';
+
+function cor(){
+    var nomeCor = 'Azul';
+    document.write(this.nomeCor + '<br>' + nomeCor + '<br>');
+
+}
+
+cor();
+//document.write(nomeCor + '<br>');
